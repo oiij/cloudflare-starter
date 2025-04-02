dda729f23f64211b1237de5682e60c43c054d00a
antrioe <antrioe@gmali.com>
2025-04-02 14:57:37 +0800

    init(projects): init

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,13 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vitest/config'
+import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 
-export default defineConfig({
-  plugins: [],
+export default defineWorkersConfig({
   test: {
-    globals: true,
+    poolOptions: {
+      workers: {
+        wrangler: { configPath: './wrangler.jsonc' },
+      },
+    },
   },
 })
